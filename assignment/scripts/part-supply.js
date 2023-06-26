1a14c2b4d59ae8b7e769bc2c817a0b465629abb3
Katya Marie <katyawesely@gmail.com>
2023-06-26 15:59:03 -0500

    html - index, unordered list completed. assignment 3.3. loops, arrays and methods continued, tying everything together.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,20 +4,33 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
+let partsNeeded = 40;
+console.log('we need ' + partsNeeded + ' parts');
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11];
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+//pop() method to remove last entry added to array (shift to remove first entry)
+let removedSupply = supplyChanges.pop();
+console.log('made a mistake, removed the last entry:', removedSupply);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+
+//push to add entry to the end of a list (unshift to add to beginning)
+supplyChanges.push('25');
+
+//console message to check new entry is added 
+console.log('We\'ve added 25 parts we received: ', supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -27,7 +40,24 @@ console.log('5. Adding 25 to supplyChanges.');
 console.log('6. Showing supplyChanges...');
 
 
-// STRETCH GOALS
+for(let i = 0; i<supplyChanges.length; i++){
+    let x = supplyChanges[i];
+
+    if(x > 0){
+        console.log( 'added ' + x + ' parts');
+    } else if(x < 0){
+        console.log('removed ' + x + ' parts');
+    } else {
+        console.log('No change');
+    }
+}
+
+
+
+// STRETCH GOALS 
+// In hopes to complete the assignment assigned today due Sunday 7/1 earlier this time,
+// I will be submitting any stretch goals if / when resubmissions are asked of me! 
+// I will complete resubmissions in the next few days, by the end of the week. TY
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
